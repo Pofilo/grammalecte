0ebf95c17d5e6c31426cda5fc98e11f94ef92f99
olr <olr>
2017-07-20 15:07:24 +0000

    [fx] All-in-one panel

--- gc_lang/fr/webext/panel/main.js
+++ gc_lang/fr/webext/panel/main.js
@@ -22,9 +22,49 @@ window.addEventListener(
       if (xElem.id) {
 
       }
+    } else if (xElem.className === "select") {
+      showPage(xElem.dataset.page);
     } else if (xElem.tagName === "A") {
       openURL(xElem.getAttribute("href"));
     }
   },
   false
 );
+
+function showPage (sPageName) {
+  try {
+    // hide them all
+    for (let xNodePage of document.getElementsByClassName("page")) {
+      xNodePage.style.display = "None";
+    }
+    // show the one
+    document.getElementById(sPageName).style.display = "block";
+    sendMessage("Mon message");
+    // specific modifications
+    if (sPageName === "conj_page") {
+      document.body.style.width = "600px";
+      document.documentElement.style.width = "600px";
+      document.getElementById("movewindow").style.display = "none";
+    } else {
+      document.body.style.width = "530px";
+      document.documentElement.style.width = "530px";
+      document.getElementById("movewindow").style.display = "block";
+    }
+  }
+  catch (e) {
+    showError(e);
+  }
+}
+
+function handleResponse(message) {
+  console.log(`background script sent a response: ${message.response}`);
+}
+
+function handleError(error) {
+  console.log(`Error: ${error}`);
+}
+
+function sendMessage (sMessage) {
+  let sending = browser.runtime.sendMessage({content: sMessage});
+  sending.then(handleResponse, handleError);  
+}
