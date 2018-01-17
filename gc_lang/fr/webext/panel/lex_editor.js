affa5053435df07dcdeeeeedfc41b2c997ef4ec3
olr <olr>
2018-01-17 17:37:49 +0000

    [fx] update: lexicon editor

--- gc_lang/fr/webext/panel/lex_editor.js
+++ gc_lang/fr/webext/panel/lex_editor.js
@@ -13,7 +13,7 @@ function onSelectionClick (xEvent) {
         let xElem = xEvent.target;
         if (xElem.id) {
             if (xElem.id.startsWith("select_")) {
-                showSection("section_" + xElem.id.slice(7));
+                oPage.showSection("section_" + xElem.id.slice(7));
             } else {
                 
             }
@@ -36,17 +36,77 @@ document.getElementById("categories").addEventListener("click", onSelectionClick
     ACTIONS
 */
 
-function hideAllSections () {
-    for (let xElem of document.getElementById("editor").childNodes) {
-        if (xElem.id) {
-            xElem.style.display = "none";
+const oPage = {
+
+    hideAllSections: function () {
+        for (let xElem of document.getElementById("editor").childNodes) {
+            if (xElem.id) {
+                xElem.style.display = "none";
+            }
+        }
+    },
+
+    showSection: function (sName) {
+        this.clear();
+        this.hideAllSections();
+        if (document.getElementById(sName)) {
+            document.getElementById(sName).style.display = "block";
+        }
+    },
+
+    clear: function () {
+        try {
+            // nom, adjectif, noms propres
+            for (let xElem of document.getElementsByName("POS")) {
+                xElem.checked = false;
+            }
+            for (let xElem of document.getElementsByName("POS2")) {
+                xElem.checked = false;
+            }
+            for (let xElem of document.getElementsByName("pluriel")) {
+                xElem.checked = false;
+            }
+            for (let xElem of document.getElementsByName("genre")) {
+                xElem.checked = false;
+            }
+            // verbe
+            document.getElementById("v_i").checked = false;
+            document.getElementById("v_t").checked = false;
+            document.getElementById("v_n").checked = false;
+            document.getElementById("v_p").checked = false;
+            document.getElementById("v_m").checked = false;
+            document.getElementById("v_ae").checked = false;
+            document.getElementById("v_aa").checked = false;
+            // autre
+            document.getElementById("lemma").value = "";
+            document.getElementById("tags").value = "";
+        }
+        catch (e) {
+            showError(e);
         }
     }
 }
 
-function showSection (sName) {
-    hideAllSections();
-    if (document.getElementById(sName)) {
-        document.getElementById(sName).style.display = "block";
+
+
+const oFlex = {
+
+    sLemma: "",
+    lFlexion: [],
+
+    getLemma: function () {
+        this.sLemma = document.getElementById("lemma").value;
+    },
+
+    create: function () {
+
+    },
+
+    show: function () {
+
+    },
+
+    addToDictionary: function () {
+
     }
 }
