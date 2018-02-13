8ed3fcc726bd0d3f822efcb39af46ac2aa39c0f2
olr <olr>
2018-02-13 10:47:09 +0000

    [graphspell][py] spellchecker wrapper for ibdawg

--- graphspell/spellchecker.py
+++ graphspell/spellchecker.py
@@ -2,133 +2,115 @@
 # Wrapper for the IBDAWG class.
 # Useful to check several dictionaries at once.
 
+# To avoid iterating over a pile of dictionaries, it is assumed that 3 are enough:
+# - the main dictionary, bundled with the package
+# - the extended dictionary, added by an organization
+# - the personal dictionary, created by the user for its own convenience
+
+
+import traceback
+
 from . import ibdawg
 
 
-dDictionaries = {
-    "fr": "French.bdic",
-    "en": "English.bdic"
+dDefaultDictionaries = {
+    "fr": "fr.bdic",
+    "en": "en.bdic"
 }
 
 
 class Spellchecker ():
 
-    def __init__ (self, sLangCode):
+    def __init__ (self, sLangCode, sfMainDic="", sfExtendedDic="", sfPersonalDic=""):
+        "returns True if the main dictionary is loaded"
         self.sLangCode = sLangCode
-        self.oMainDic = None
-        if sLangCode in dDictionaries:
-            self.oMainDic = ibdawg.IBDAWG(dDictionaries[sLangCode])
-        self.lOtherDic = []
+        if not sfMainDic:
+            sfMainDic = dDefaultDictionaries.get(sLangCode, "")
+        self.oMainDic = self._loadDictionary(sfMainDic)
+        self.oExtendedDic = self._loadDictionary(sfExtendedDic)
+        self.oPersonalDic = self._loadDictionary(sfPersonalDic)
         return bool(self.oMainDic)
 
-
-    def setMainDictionary (self, sDicName):
+    def _loadDictionary (self, sfDictionary):
+        "returns an IBDAWG object"
+        if not sfDictionary:
+            return None
         try:
-            self.oMainDic = ibdawg.IBDAWG(sDicName)
-            return True
+            return ibdawg.IBDAWG(sfDictionary)
         except:
-            print("Error: <" + sDicName + "> not set as main dictionary.")
-            return False
+            print("Error: <" + sDicName + "> not loaded.")
+            traceback.print_exc()
+            return None
 
-    def addDictionary (self, sDicName):
-        try:
-            self.lOtherDic.append(ibdawg.IBDAWG(sDicName))
-            return True
-        except:
-            print("Error: <" + sDicName + "> not added to the list.")
-            return False
+    def setMainDictionary (self, sfDictionary):
+        "returns True if the dictionary is loaded"
+        self.oMainDic = self._loadDictionary(sfDictionary)
+        return bool(self.oMainDic)
+            
+    def setExtendedDictionary (self, sfDictionary):
+        "returns True if the dictionary is loaded"
+        self.oExtendedDic = self._loadDictionary(sfDictionary)
+        return bool(self.oExtendedDic)
 
-    # Return codes:
-    #   0: invalid
-    #   1: correct in main dictionary
-    #   2+: correct in foreign dictionaries
+    def setPersonalDictionary (self, sfDictionary):
+        "returns True if the dictionary is loaded"
+        self.oPersonalDic = self._loadDictionary(sfDictionary)
+        return bool(self.oPersonalDic)
 
 
-    # check in the main dictionary only
+    # IBDAWG functions
 
     def isValidToken (self, sToken):
-        "(in main dictionary) checks if sToken is valid (if there is hyphens in sToken, sToken is split, each part is checked)"
+        "checks if sToken is valid (if there is hyphens in sToken, sToken is split, each part is checked)"
         if self.oMainDic.isValidToken(sToken):
-            return 1
-        return 0
+            return True
+        if self.oExtendedDic and self.oExtendedDic.isValidToken(sToken):
+            return True
+        if self.oPersonalDic and self.oPersonalDic.isValidToken(sToken):
+            return True
+        return False
 
     def isValid (self, sWord):
-        "(in main dictionary) checks if sWord is valid (different casing tested if the first letter is a capital)"
-        if self.oMainDic.isValid(sWord):
-            return 1
-        return 0
+        "checks if sWord is valid (different casing tested if the first letter is a capital)"
+        if self.oMainDic.isValid(sToken):
+            return True
+        if self.oExtendedDic and self.oExtendedDic.isValid(sToken):
+            return True
+        if self.oPersonalDic and self.oPersonalDic.isValid(sToken):
+            return True
+        return False
 
     def lookup (self, sWord):
-        "(in main dictionary) checks if sWord is in dictionary as is (strict verification)"
-        if self.oMainDic.lookup(sWord):
-            return 1
-        return 0
-
-
-    # check in all dictionaries
-
-    def isValidTokenAll (self, sToken):
-        "(in all dictionaries) checks if sToken is valid (if there is hyphens in sToken, sToken is split, each part is checked)"
-        if self.oMainDic.isValidToken(sToken):
-            return 1
-        for i, oDic in enumerate(self.lOtherDic, 2):
-            if oDic.isValidToken(sToken):
-                return i
-        return 0
-
-    def isValidAll (self, sWord):
-        "(in all dictionaries) checks if sWord is valid (different casing tested if the first letter is a capital)"
-        if self.oMainDic.isValid(sToken):
-            return 1
-        for i, oDic in enumerate(self.lOtherDic, 2):
-            if oDic.isValid(sToken):
-                return i
-        return 0
-
-    def lookupAll (self, sWord):
-        "(in all dictionaries) checks if sWord is in dictionary as is (strict verification)"
+        "checks if sWord is in dictionary as is (strict verification)"
         if self.oMainDic.lookup(sToken):
-            return 1
-        for i, oDic in enumerate(self.lOtherDic, 2):
-            if oDic.lookup(sToken):
-                return i
-        return 0
+            return True
+        if self.oExtendedDic and self.oExtendedDic.lookup(sToken):
+            return True
+        if self.oPersonalDic and self.oPersonalDic.lookup(sToken):
+            return True
+        return False
 
+    def getMorph (self, sWord):
+        "retrieves morphologies list, different casing allowed"
+        lResult = self.oMainDic.getMorph(sToken)
+        if self.oExtendedDic:
+            lResult.extends(self.oExtendedDic.getMorph(sToken))
+        if self.oPersonalDic:
+            lResult.extends(self.oPersonalDic.getMorph(sToken))
+        return lResult
 
-    # check in dictionaries up to level n
+    def suggest (self, sWord, nSuggLimit=10):
+        "generator: returns 1,2 or 3 lists of suggestions"
+        yield self.oMainDic.suggest(sWord, nSuggLimit)
+        if self.oExtendedDic:
+            yield self.oExtendedDic.suggest(sWord, nSuggLimit)
+        if self.oPersonalDic:
+            yield self.oPersonalDic.suggest(sWord, nSuggLimit)
 
-    def isValidTokenLevel (self, sToken, nLevel):
-        "(in dictionaries up to level n) checks if sToken is valid (if there is hyphens in sToken, sToken is split, each part is checked)"
-        if self.oMainDic.isValidToken(sToken):
-            return 1
-        if nLevel >= 2:
-            for i, oDic in enumerate(self.lOtherDic, 2):
-                if oDic.isValidToken(sToken):
-                    return i
-                if i == nLevel:
-                    break
-        return 0
-
-    def isValidLevel (self, sWord, nLevel):
-        "(in dictionaries up to level n) checks if sWord is valid (different casing tested if the first letter is a capital)"
-        if self.oMainDic.isValid(sToken):
-            return 1
-        if nLevel >= 2:
-            for i, oDic in enumerate(self.lOtherDic, 2):
-                if oDic.isValid(sToken):
-                    return i
-                if i == nLevel:
-                    break
-        return 0
-
-    def lookupLevel (self, sWord, nLevel):
-        "(in dictionaries up to level n) checks if sWord is in dictionary as is (strict verification)"
-        if self.oMainDic.lookup(sToken):
-            return 1
-        if nLevel >= 2:
-            for i, oDic in enumerate(self.lOtherDic, 2):
-                if oDic.lookup(sToken):
-                    return i
-                if i == nLevel:
-                    break
-        return 0
+    def select (self, sPattern=""):
+        "generator: returns all entries which morphology fits <sPattern>"
+        yield from self.oMainDic.select(sPattern)
+        if self.oExtendedDic:
+            yield from self.oExtendedDic.select(sPattern)
+        if self.oPersonalDic:
+            yield from self.oPersonalDic.select(sPattern)
