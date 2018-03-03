a6f6a75ff60cecdd33636363860e5156611c7bfb
olr <olr>
2018-03-03 06:47:43 +0000

    [tb][bug] fix use of spellchecking

--- gc_lang/fr/tb/worker/gce_worker.js
+++ gc_lang/fr/tb/worker/gce_worker.js
@@ -85,7 +85,7 @@ function parse (sText, sCountry, bDebug, bContext) {
 
 function parseAndSpellcheck (sText, sCountry, bDebug, bContext) {
     let aGrammErr = gce.parse(sText, sCountry, bDebug, bContext);
-    let aSpellErr = oTokenizer.getSpellingErrors(sText, oSpellChecker);
+    let aSpellErr = oSpellChecker.parseParagraph(sText);
     return JSON.stringify({ aGrammErr: aGrammErr, aSpellErr: aSpellErr });
 }
 
