db430d81f38d9c5368157577243861623e3f8c02
olr <olr>
2018-06-24 12:19:32 +0000

    [core] grammar checker: code cleaning (pylint)

--- gc_core/py/grammar_checker.py
+++ gc_core/py/grammar_checker.py
@@ -1,5 +1,6 @@
-# Grammalecte
-# Main class: wrapper
+"""
+Grammalecte, grammar checker
+"""
 
 import importlib
 import json
@@ -8,6 +9,7 @@ from . import text
 
 
 class GrammarChecker:
+    "GrammarChecker: Wrapper for the grammar checker engine"
 
     def __init__ (self, sLangCode, sContext="Python"):
         self.sLangCode = sLangCode
@@ -22,24 +24,29 @@ class GrammarChecker:
         self.oTextFormatter = None
 
     def getGCEngine (self):
+        "return the grammar checker object"
         return self.gce
 
     def getSpellChecker (self):
+        "return the spell checker object"
         return self.oSpellChecker
 
     def getTextFormatter (self):
-        if self.oTextFormatter == None:
-            self.tf = importlib.import_module("."+self.sLangCode+".textformatter", "grammalecte")
-        self.oTextFormatter = self.tf.TextFormatter()
+        "load and return the text formatter"
+        if self.oTextFormatter is None:
+            tf = importlib.import_module("."+self.sLangCode+".textformatter", "grammalecte")
+            self.oTextFormatter = tf.TextFormatter()
         return self.oTextFormatter
 
     def getLexicographer (self):
-        if self.oLexicographer == None:
-            self.lxg = importlib.import_module("."+self.sLangCode+".lexicographe", "grammalecte")
-        self.oLexicographer = self.lxg.Lexicographe(self.oSpellChecker)
+        "load and return the lexicographer"
+        if self.oLexicographer is None:
+            lxg = importlib.import_module("."+self.sLangCode+".lexicographe", "grammalecte")
+            self.oLexicographer = lxg.Lexicographe(self.oSpellChecker)
         return self.oLexicographer
 
     def displayGCOptions (self):
+        "display the grammar checker options"
         self.gce.displayOptions()
 
     def getParagraphErrors (self, sText, dOptions=None, bContext=False, bSpellSugg=False, bDebug=False):
@@ -49,18 +56,22 @@ class GrammarChecker:
         return aGrammErrs, aSpellErrs
 
     def generateText (self, sText, bEmptyIfNoErrors=False, bSpellSugg=False, nWidth=100, bDebug=False):
+        "[todo]"
         pass
 
     def generateTextAsJSON (self, sText, bContext=False, bEmptyIfNoErrors=False, bSpellSugg=False, bReturnText=False, bDebug=False):
+        "[todo]"
         pass
 
     def generateParagraph (self, sText, dOptions=None, bEmptyIfNoErrors=False, bSpellSugg=False, nWidth=100, bDebug=False):
+        "parse text and return a readable text with underline errors"
         aGrammErrs, aSpellErrs = self.getParagraphErrors(sText, dOptions, False, bSpellSugg, bDebug)
         if bEmptyIfNoErrors and not aGrammErrs and not aSpellErrs:
             return ""
         return text.generateParagraph(sText, aGrammErrs, aSpellErrs, nWidth)
 
     def generateParagraphAsJSON (self, iIndex, sText, dOptions=None, bContext=False, bEmptyIfNoErrors=False, bSpellSugg=False, bReturnText=False, lLineSet=None, bDebug=False):
+        "parse text and return errors as a JSON string"
         aGrammErrs, aSpellErrs = self.getParagraphErrors(sText, dOptions, bContext, bSpellSugg, bDebug)
         aGrammErrs = list(aGrammErrs)
         if bEmptyIfNoErrors and not aGrammErrs and not aSpellErrs:
