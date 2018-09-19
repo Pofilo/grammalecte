982ca188f04edd6b3dfa86fdfd483a5d178ea20b
olr <olr>
2018-09-19 18:47:22 +0000

    [core][js] add forgotten file <gc_rules_graph.js> to the repo

---
+++ gc_core/js/lang_core/gc_rules_graph.js
@@ -0,0 +1,20 @@
+// Grammar checker graph rules
+/*jslint esversion: 6*/
+/*global exports*/
+
+"use strict";
+
+${string}
+
+
+var gc_rules_graph = {
+    dAllGraph: ${rules_graphsJS},
+
+    dRule: ${rules_actionsJS}
+}
+
+
+if (typeof(exports) !== 'undefined') {
+    exports.dAllGraph = gc_rules_graph.dAllGraph;
+    exports.dRule = gc_rules_graph.dRule;
+}
