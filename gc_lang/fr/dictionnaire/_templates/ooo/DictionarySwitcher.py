8d603019eb96e011c9c5b3045a5f7e027817c98e
olr <olr>
2018-10-30 00:22:24 +0000

    [fr] Extension dictionnaires: mise à jour du sélecteur

--- gc_lang/fr/dictionnaire/_templates/ooo/DictionarySwitcher.py
+++ gc_lang/fr/dictionnaire/_templates/ooo/DictionarySwitcher.py
@@ -47,7 +47,7 @@ class FrenchDictionarySwitcher (unohelper.Base, XActionListener, XJobExecutor):
             self.dUI = ds_strings.dStrings[sLocale[0:2]]
         else:
             self.dUI = ds_strings.dStrings["fr"]
-        
+
     def addWidget (self, name, wtype, x, y, w, h, **kwargs):
         xWidget = self.dialog.createInstance('com.sun.star.awt.UnoControl%sModel' % wtype)
         xWidget.Name = name
@@ -66,59 +66,59 @@ class FrenchDictionarySwitcher (unohelper.Base, XActionListener, XJobExecutor):
         xLocations = xSettings.getByName("Locations")
         m = re.search(r"fr-(\w*)\.(?:dic|aff)", xLocations[0])
         self.sCurrentDic = m.group(1)
-        
+
         # dialog
         self.dialog = self.xSvMgr.createInstanceWithContext('com.sun.star.awt.UnoControlDialogModel', self.ctx)
         self.dialog.Width = 200
-        self.dialog.Height = 290
+        self.dialog.Height = 230
         self.dialog.Title = self.dUI.get('title', "#title#")
         xWindowSize = getWindowSize()
         self.dialog.PositionX = int((xWindowSize.Width / 2) - (self.dialog.Width / 2))
         self.dialog.PositionY = int((xWindowSize.Height / 2) - (self.dialog.Height / 2))
-        
+
         # xWidgets
         padding = 10
         hspace = 60
-        posY1 = 20; posY2 = posY1 + hspace; posY3 = posY2 + hspace; posY4 = posY3 + hspace; posY5 = posY4 + hspace + 10;
+        posY1 = 20; posY2 = posY1 + hspace; posY3 = posY2 + hspace; posY4 = posY3 + hspace + 10
         nLblWidth = 170
         nLblHeight = 20
         nDescWidth = 170
         nDescHeight = 40
-        
+
         xFD1 = uno.createUnoStruct("com.sun.star.awt.FontDescriptor")
         xFD1.Height = 12
         xFD1.Name = "Verdana"
-        
+
         xFD2 = uno.createUnoStruct("com.sun.star.awt.FontDescriptor")
         xFD2.Height = 11
         xFD2.Name = "Verdana"
-        
+
         xFD3 = uno.createUnoStruct("com.sun.star.awt.FontDescriptor")
         xFD3.Height = 10
         xFD3.Weight = uno.getConstantByName("com.sun.star.awt.FontWeight.BOLD")
         xFD3.Name = "Verdana"
-        
-        gbm = self.addWidget('groupbox', 'GroupBox', 5, 5, 190, 260, Label = self.dUI.get('choose', "#choose#"), FontDescriptor = xFD1, FontRelief = 1, TextColor = 0xAA2200)
-        
+
+        gbm = self.addWidget('groupbox', 'GroupBox', 5, 5, 190, 200, Label = self.dUI.get('choose', "#choose#"), FontDescriptor = xFD1, FontRelief = 1, TextColor = 0xAA2200)
+
         # Note: the only way to group RadioButtons is to create them successively
-        rbm_m = self.addWidget('rb-moderne', 'RadioButton', padding, posY1, nLblWidth, nLblHeight, Label = self.dUI.get('moderne', "#moderne#"), FontDescriptor = xFD2, FontRelief = 1, TextColor = 0x0022AA)
-        self.xRB_m = self.xSvMgr.createInstanceWithContext('com.sun.star.awt.UnoControlRadioButton', self.ctx)
-        self.xRB_m.setModel(rbm_m)
-        rbm_c = self.addWidget('rb-classique', 'RadioButton', padding, posY2, nLblWidth, nLblHeight, Label = self.dUI.get('classique', "#classique#"), FontDescriptor = xFD2, FontRelief = 1, TextColor = 0x0022AA)
+        #rbm_m = self.addWidget('rb-moderne', 'RadioButton', padding, posY1, nLblWidth, nLblHeight, Label = self.dUI.get('moderne', "#moderne#"), FontDescriptor = xFD2, FontRelief = 1, TextColor = 0x0022AA)
+        #self.xRB_m = self.xSvMgr.createInstanceWithContext('com.sun.star.awt.UnoControlRadioButton', self.ctx)
+        #self.xRB_m.setModel(rbm_m)
+        rbm_c = self.addWidget('rb-classique', 'RadioButton', padding, posY1, nLblWidth, nLblHeight, Label = self.dUI.get('classique', "#classique#"), FontDescriptor = xFD2, FontRelief = 1, TextColor = 0x0022AA)
         self.xRB_c = self.xSvMgr.createInstanceWithContext('com.sun.star.awt.UnoControlRadioButton', self.ctx)
         self.xRB_c.setModel(rbm_c)
-        rbm_r = self.addWidget('rb-reforme1990', 'RadioButton', padding, posY3, nLblWidth, nLblHeight, Label = self.dUI.get('reforme1990', "#reforme1990#"), FontDescriptor = xFD2, FontRelief = 1, TextColor = 0x0022AA)
+        rbm_r = self.addWidget('rb-reforme1990', 'RadioButton', padding, posY2, nLblWidth, nLblHeight, Label = self.dUI.get('reforme1990', "#reforme1990#"), FontDescriptor = xFD2, FontRelief = 1, TextColor = 0x0022AA)
         self.xRB_r = self.xSvMgr.createInstanceWithContext('com.sun.star.awt.UnoControlRadioButton', self.ctx)
         self.xRB_r.setModel(rbm_r)
-        rbm_t = self.addWidget('rb-toutesvariantes', 'RadioButton', padding, posY4, nLblWidth, nLblHeight, Label = self.dUI.get('toutesvariantes', "#toutesvariantes#"), FontDescriptor = xFD2, FontRelief = 1, TextColor = 0x0022AA)
+        rbm_t = self.addWidget('rb-toutesvariantes', 'RadioButton', padding, posY3, nLblWidth, nLblHeight, Label = self.dUI.get('toutesvariantes', "#toutesvariantes#"), FontDescriptor = xFD2, FontRelief = 1, TextColor = 0x0022AA)
         self.xRB_t = self.xSvMgr.createInstanceWithContext('com.sun.star.awt.UnoControlRadioButton', self.ctx)
         self.xRB_t.setModel(rbm_t)
-        
-        label_m = self.addWidget('label_m', 'FixedText', 20, posY1+10, nDescWidth, nDescHeight, Label = self.dUI.get('descModern', "#descModern#"), MultiLine = True)
-        label_c = self.addWidget('label_c', 'FixedText', 20, posY2+10, nDescWidth, nDescHeight, Label = self.dUI.get('descClassic', "#descClassic#"), MultiLine = True)
-        label_r = self.addWidget('label_r', 'FixedText', 20, posY3+10, nDescWidth, nDescHeight, Label = self.dUI.get('descReform', "#descReform#"), MultiLine = True)
-        label_t = self.addWidget('label_t', 'FixedText', 20, posY4+10, nDescWidth, nDescHeight, Label = self.dUI.get('descAllvar', "#descAllvar#"), MultiLine = True)
-        
+
+        #label_m = self.addWidget('label_m', 'FixedText', 20, posY1+10, nDescWidth, nDescHeight, Label = self.dUI.get('descModern', "#descModern#"), MultiLine = True)
+        label_c = self.addWidget('label_c', 'FixedText', 20, posY1+10, nDescWidth, nDescHeight, Label = self.dUI.get('descClassic', "#descClassic#"), MultiLine = True)
+        label_r = self.addWidget('label_r', 'FixedText', 20, posY2+10, nDescWidth, nDescHeight, Label = self.dUI.get('descReform', "#descReform#"), MultiLine = True)
+        label_t = self.addWidget('label_t', 'FixedText', 20, posY3+10, nDescWidth, nDescHeight, Label = self.dUI.get('descAllvar', "#descAllvar#"), MultiLine = True)
+
         if self.sCurrentDic:
             self.setRadioButton(self.sCurrentDic)
             sMsgLabel = self.dUI.get('restart', "#restart#")
@@ -126,10 +126,10 @@ class FrenchDictionarySwitcher (unohelper.Base, XActionListener, XJobExecutor):
         else:
             sMsgLabel = self.dUI.get('error', "#error#")
             bButtonActive = False
-        
-        label_info = self.addWidget('label_info', 'FixedText', 10, posY4+50, 180, 10, Label = sMsgLabel, TextColor = 0xAA2200, MultiLine = True)
-        button = self.addWidget('select', 'Button', padding+40, posY5, 100, 14, Label = self.dUI.get('select', "#select#"), FontDescriptor = xFD3, TextColor = 0x004400, Enabled = bButtonActive)
-        
+
+        label_info = self.addWidget('label_info', 'FixedText', 10, posY3+50, 180, 10, Label = sMsgLabel, TextColor = 0xAA2200, MultiLine = True)
+        button = self.addWidget('select', 'Button', padding+40, posY4, 100, 14, Label = self.dUI.get('select', "#select#"), FontDescriptor = xFD3, TextColor = 0x004400, Enabled = bButtonActive)
+
         # container
         self.xContainer = self.xSvMgr.createInstanceWithContext('com.sun.star.awt.UnoControlDialog', self.ctx)
         self.xContainer.setModel(self.dialog)
@@ -138,10 +138,11 @@ class FrenchDictionarySwitcher (unohelper.Base, XActionListener, XJobExecutor):
         toolkit = self.xSvMgr.createInstanceWithContext('com.sun.star.awt.ExtToolkit', self.ctx)
         self.xContainer.createPeer(toolkit, None)
         self.xContainer.execute()
-    
+
     def setRadioButton (self, sDic):
         if sDic == 'moderne':
-            self.xRB_m.setState(True)
+            #self.xRB_m.setState(True)
+            self.xRB_c.setState(True)
         elif sDic == 'classique':
             self.xRB_c.setState(True)
         elif sDic == 'reforme1990':
@@ -150,12 +151,12 @@ class FrenchDictionarySwitcher (unohelper.Base, XActionListener, XJobExecutor):
             self.xRB_t.setState(True)
         else:
             pass
-        
+
     def actionPerformed (self, actionEvent):
         try:
-            if self.xRB_m.getState():
-                self.sSelectedDic = 'moderne'
-            elif self.xRB_c.getState():
+            #if self.xRB_m.getState():
+            #    self.sSelectedDic = 'moderne'
+            if self.xRB_c.getState():
                 self.sSelectedDic = 'classique'
             elif self.xRB_r.getState():
                 self.sSelectedDic = 'reforme1990'
@@ -167,7 +168,7 @@ class FrenchDictionarySwitcher (unohelper.Base, XActionListener, XJobExecutor):
             self.xContainer.endExecute()
         except:
             traceback.print_exc()
-    
+
     def trigger (self, args):
         try:
             dialog = FrenchDictionarySwitcher(self.ctx)
--- gc_lang/fr/dictionnaire/_templates/ooo/ds_strings.py
+++ gc_lang/fr/dictionnaire/_templates/ooo/ds_strings.py
@@ -5,16 +5,14 @@ dStrings = {
             "title": u"Orthographe française",
             "choose": u"Choisissez un dictionnaire",
             "select": u"Utiliser ce dictionnaire",
-            
-            "moderne": u"“Moderne”",
+
             "classique": u"“Classique” (recommandé)",
             "reforme1990": u"“Réforme 1990”",
             "toutesvariantes": u"“Toutes variantes”",
 
-            "descModern": u"Ce dictionnaire propose l’orthographe telle qu’elle est écrite aujourd’hui le plus couramment. C’est le dictionnaire recommandé si le français n’est pas votre langue maternelle ou si vous ne désirez qu’une seule graphie correcte par mot.",
-            "descClassic": u"Il s’agit du dictionnaire “Moderne”, avec des graphies classiques en sus, certaines encore communément utilisées, d’autres désuètes. C’est le dictionnaire recommandé si le français est votre langue maternelle.",
-            "descReform": u"Avec ce dictionnaire, seule l’orthographe réformée est reconnue. Attendu que bon nombre de graphies réformées sont considérées comme erronées par beaucoup, ce dictionnaire est déconseillé. Les graphies passées dans l’usage sont déjà incluses dans le dictionnaire “Moderne”.",
-            "descAllvar": u"Ce dictionnaire contient les variantes graphiques, classiques, réformées, ainsi que d’autres plus rares encore. Ce dictionnaire est déconseillé à ceux qui ne connaissent pas très bien la langue française.",
+            "descClassic": u"Ce dictionnaire propose l’orthographe telle qu’elle est écrite aujourd’hui le plus couramment. Toutefois, ont été intégrées quelques graphies acceptées par la réforme principalement pour des raisons de cohérence. Il s’agit de graphies rectifiant des erreurs ou des oublis dans les réformes précédentes.",
+            "descReform": u"Avec ce dictionnaire, seule l’orthographe réformée est reconnue. Attendu que bon nombre de graphies réformées sont considérées comme erronées par beaucoup, ce dictionnaire est déconseillé. Les graphies passées dans l’usage sont déjà incluses dans le dictionnaire “Classique”.",
+            "descAllvar": u"Ce dictionnaire contient les variantes graphiques classiques et “réformées”. Ce dictionnaire est déconseillé à ceux qui ne connaissent pas très bien la langue française.",
 
             "restart": u"Le changement ne sera effectif qu’après le redémarrage du logiciel.",
             "error": u"Erreur : impossible de savoir quel dictionnaire est actif."
@@ -23,16 +21,14 @@ dStrings = {
             "title": u"French spelling",
             "choose": u"Choose a dictionary",
             "select": u"Use this dictionary",
-            
-            "moderne": u"“Modern”",
+
             "classique": u"“Classic” (recommended)",
             "reforme1990": u"“Reform 1990”",
             "toutesvariantes": u"“All variants”",
-            
-            "descModern": u"This dictionary offers the French spelling as it is written nowadays most often. This is the recommended dictionary if French is not your mother tongue or if you want only one correct spelling per word.",
-            "descClassic": u"This is the “Modern” dictionary plus classical spellings, some of them still widely used, others obsolete. This is the recommended dictionary if French is your native language.",
-            "descReform": u"With this dictionary, only the reformed spelling is recognized. As many of reformed spellings are considered erronous for many people, this dictionary is unadvised. Reformed spellings commonly used are already included in the “Modern” dictionary.",
-            "descAllvar": u"This dictionary contains all spelling variants, classical and reformed, and some others even rarer. This dictionary is unadvised for those who doesn’t know very well the French language.",
+
+            "descClassic": u"This dictionary offers the French spelling as it is written nowadays most commonly. However, some of the spellings accepted by the reform have been incorporated mainly for reasons of coherence. These are spellings that rectify errors or omissions in previous reforms.",
+            "descReform": u"With this dictionary, only the reformed spelling is recognized. As many of reformed spellings are considered erronous by many people, this dictionary is unadvised. Reformed spellings commonly used are already included in the “Classic” dictionary.",
+            "descAllvar": u"This dictionary contains all spelling variants, classical and reformed. This dictionary is unadvised for those who don’t know very well the French language.",
 
             "restart": u"The modification will be effective only after restarting the software.",
             "error": u"Error: enable to retrieve information about the current dictionary."
