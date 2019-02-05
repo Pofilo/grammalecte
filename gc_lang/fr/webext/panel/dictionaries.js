9cd2e6d84b0d8772a09011d55500cd9c6686b2bf
olr <olr>
2019-02-05 12:55:26 +0000

    [fx] set community dictionary as soon as it is created

--- gc_lang/fr/webext/panel/dictionaries.js
+++ gc_lang/fr/webext/panel/dictionaries.js
@@ -307,6 +307,7 @@ class Table {
         console.log(oDict);
         browser.storage.local.set({ "community_dictionary": oDict });
         browser.storage.local.set({ "selected_dictionaries_list": Array.from(this.dSelectedDictionaries.keys()) });
+        browser.runtime.sendMessage({ sCommand: "setDictionary", dParam: {sDictionary: "community", oDict: oDict}, dInfo: {} });
     }
 }
 
