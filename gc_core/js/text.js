cfbdaed172f6e9706763dc1d12a2ae60457b606a
olr <olr>
2019-05-26 08:12:51 +0000

    [core] text: sentence splitter update

--- gc_core/js/text.js
+++ gc_core/js/text.js
@@ -9,7 +9,7 @@
 
 var text = {
 
-    _zEndOfSentence: new RegExp ('[.?!:;…]+[   ]+[»”’]?(?=[«"“‘–—   ]?[A-ZÉÈÎÔ])', "g"),
+    _zEndOfSentence: new RegExp ('[.?!:;…]+[   ]+[»”’]?(?=[«"“‘–—   ]?[A-ZÀÂÉÈÊÎÔÇ])', "g"),
 
     getSentenceBoundaries: function* (sText) {
         // generator: returns start and end of sentences found in <sText>
