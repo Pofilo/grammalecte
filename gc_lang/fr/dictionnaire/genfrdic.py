59d02757dcb09a38137e53a56adc8134bc8bdbb3
olr <olr>
2020-12-02 13:40:12 +0000

    [fr] mise à jour des dictionnaires, nouveaux tags, ajustements

--- gc_lang/fr/dictionnaire/genfrdic.py
+++ gc_lang/fr/dictionnaire/genfrdic.py
@@ -1145,7 +1145,7 @@ class Flexion:
         # LEX
         "symb": ";S", "unit": ";U", "col": ";C", "hm": ";É", "pel": ";é",
         # SEM
-        "gent": ";G", "lang": ";L"
+        "gent": "#G", "lang": "#L", "cité": "#C", "pays": "#P"
     }
 
     def _getSimpleTags (self):
